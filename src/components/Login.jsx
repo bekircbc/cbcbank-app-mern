468f3e06ab62e840286781d4c1c3b071b551c0cf
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-28 00:57:01 +0200

    currnetAccount, isLoggedInHandler added..

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,45 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { AppContext } from "../AppContext";
-// import Forms from "react-forms";
 
 export const Login = () => {
   const { data } = useContext(AppContext);
+
+  const idInputRef = useRef();
+  const pinInputRef = useRef();
+  function isLoggedInHandler(event) {
+    event.preventDefault();
+    const enteredId = titleInputRef.current.value;
+    const enteredPin = imageInputRef.current.value;
+
+    const currentAccount = data.logindata.find(
+      (m) => m.id === enteredId && m.pin === enteredPin
+    );
+
+    fetch(
+      "https://basic-streaming-app-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
   return (
     <>
       <h1>Login</h1>
-      {data.accounts.map((m) => {
-        return (
-          <>
-            <p>{m.movements}</p>
-          </>
-        );
-      })}
+      {isLoggedIn ? (
+        <h2>AA</h2>
+      ) : (
+        <form className="formCard" onSubmit={submitHandler}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+
+          <button className="btnAddMeetup">Add Meetup</button>
+        </form>
+      )}
     </>
   );
 };
